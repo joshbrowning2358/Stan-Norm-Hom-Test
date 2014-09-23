eb8c370dacf09dc378d1ab394f9aa303bd7a222e
Joshua Montgomery Browning <jbrownin@ch120-05.mines.edu>
2014-09-23 17:20:19 -0600

    Package build

---
+++ snht.Rcheck/snht-Ex.R
@@ -0,0 +1,45 @@
+pkgname <- "snht"
+source(file.path(R.home("share"), "R", "examples-header.R"))
+options(warn = 1)
+library('snht')
+
+base::assign(".oldSearch", base::search(), pos = 'CheckExEnv')
+cleanEx()
+nameEx("snht")
+### * snht
+
+flush(stderr()); flush(stdout())
+
+### Name: snht
+### Title: Standard Normal Homogeneity Test
+### Aliases: snht robustSNHT robustSNHTunequal
+### Keywords: ~snht ~homogeneity
+
+### ** Examples
+
+data = rnorm(1000)
+brk = sample(1000, size=1)
+data[1:brk] = data[1:brk]-2
+out = snht( data, period=50, robust=FALSE, useC=FALSE )
+summary(out)
+
+data = rnorm(1000)
+time = 1:1000 + rnorm(1000)
+brk = sample(1000, size=1)
+data[1:brk] = data[1:brk]-2
+out = snht( data, period=50, time=time, robust=FALSE, useC=FALSE )
+summary(out)
+
+
+
+### * <FOOTER>
+###
+options(digits = 7L)
+base::cat("Time elapsed: ", proc.time() - base::get("ptime", pos = 'CheckExEnv'),"\n")
+grDevices::dev.off()
+###
+### Local variables: ***
+### mode: outline-minor ***
+### outline-regexp: "\\(> \\)?### [*]+" ***
+### End: ***
+quit('no')
